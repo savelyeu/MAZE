31b633e072134329233faab908fbe14d8eb3ea88
Artsiom Savelyeu <42652308+savelyeu@users.noreply.github.com>
2023-02-24 10:16:30 +0300

    Add files via upload

---
+++ Generator.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject Zero;
+    public GameObject End;
+    public GameObject Road;
+    public GameObject[] Buildings;
+    public GameObject[] Trees;
+
+
+    void Start()
+    {
+        
+    }
+
+}
